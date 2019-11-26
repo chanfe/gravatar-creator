97f73ca1f900a206fe6a134a687f2a088b7dd623
Felix Chan <felixchan29@gmail.com>
2019-11-26 17:51:42 -0500

    gravitar init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleSubmit(e) {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("identicon-form")
+  form.addEventListener("submit", handleSubmit)
+})
