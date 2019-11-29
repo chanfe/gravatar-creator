8324ca3960372f6121800f5172b7478846780005
Felix Chan <felixchan29@gmail.com>
2019-11-29 10:22:04 -0500

    separate functionablity

--- src/index.js
+++ src/index.js
@@ -5,39 +5,48 @@ function handleSubmit(e) {
   e.target.reset()
 
 }
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * Math.floor(max));
-// }
+
+function updateDOM(input){
+  let identicon = new Identicon(input)
+  let defColor = `rgb(255,255,255)`
+  let c = 0
+  for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 3; j++){
+      if(identicon.uniqueInt[c] < 127){
+        document.getElementById(`${i}-${j}`).style.backgroundColor = identicon.color
+        document.getElementById(`${i}-${4-j}`).style.backgroundColor = identicon.color
+      }
+      else{
+        document.getElementById(`${i}-${j}`).style.backgroundColor = defColor
+        document.getElementById(`${i}-${4-j}`).style.backgroundColor = defColor
+      }
+      c++;
+    }
+  }
+  console.log(identicon)
+
+}
+
+class Identicon {
+  // make use of the md5.array method, which is in scope because we required it in our index.html
+  constructor(user) {
+    this.user = user;
+    this.uniqueInt = this._user_value()
+    this.color = this._rbg_color();
+  }
+
+  _user_value(){
+    return md5.array(this.user)
+  }
+
+  _rbg_color() {
+    return `rgb(${this.uniqueInt[0]},${this.uniqueInt[1]},${this.uniqueInt[2]})`
+  }
+  
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
 })
 
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("identicon-form")
-  form.addEventListener("submit", a => {
-    a.preventDefault()
-    //let blockColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
-    //let defColor = `rgb(${getRandomInt(256)},${getRandomInt(256)},${getRandomInt(256)})`
-    let defColor = `rgb(255,255,255)`
-    let text_value = a.target[0].value
-    const hashedArr = md5.array(text_value)
-    let blockColor = `rgb(${hashedArr[0]},${hashedArr[1]},${hashedArr[2]})`
-    let c = 0
-    for(let i = 0; i < 5; i++){
-      for(let j = 0; j < 3; j++){
-        if(hashedArr[c] < 127){
-          document.getElementById(`${i}-${j}`).style.backgroundColor = blockColor
-          document.getElementById(`${i}-${4-j}`).style.backgroundColor = blockColor
-        }
-        else{
-          document.getElementById(`${i}-${j}`).style.backgroundColor = defColor
-          document.getElementById(`${i}-${4-j}`).style.backgroundColor = defColor
-        }
-        c+=1
-      }
-    }
-
-  })
-})
